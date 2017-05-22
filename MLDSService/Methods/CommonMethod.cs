fb9f99ff3dea8349b839921011dbdbb1f6cbc54c
shenjunyu <1037420932@qq.com>
2017-05-22 19:51:39 +0800

    Add files via upload

---
+++ MLDSService/Methods/CommonMethod.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MLDSData;
+
+namespace MLDSService.Methods
+{
+    public static class CommonMethod
+    {
+        private static MLDSDataContext ds = new MLDSDataContext();
+        /*-------------身份证计算年龄---------------*/
+        public static int IDCard2Age(string IDCard)
+        {
+            string Sub_str = IDCard.Substring(6, 8).Insert(4, "-").Insert(7, "-");   //提取出生年份
+            TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(Sub_str));
+            return (ts.Days / 365);
+        }
+
+        /*-------------计算党龄---------------*/
+        public static int joinTime2partyAge(string joinTime)
+        {
+            TimeSpan ts = DateTime.Now.Subtract(Convert.ToDateTime(joinTime));
+            return (ts.Days / 365);
+        }
+
+        /*--------------计算下属区域----------*/
+        public static List<string> getSubDistrict(string districtID)
+        {
+            List<string> districts = new List<string>();
+            List<string> dis1 = new List<string>();
+            List<string> dis2 = new List<string>();
+            districts.Add(districtID);
+            dis2.Add(districtID);
+            try
+            {
+                SYS_District[] x = null;
+                SYS_District[] y = null;
+
+                do
+                {
+                    dis1.Clear();
+                    for (int i = 0; i < dis2.Count(); i++)
+                    {
+                        x = (from a in ds.SYS_District
+                             where a.attachTo == dis2[i]
+                             select a).ToArray();
+                        for (int j = 0; j < x.Length; j++)
+                        {
+                            if (districts.Contains(x[j].id) == false)
+                            {
+                                districts.Add(x[j].id);
+                                dis2.Add(x[j].id);
+                            }
+                        }
+                        dis2.Remove(dis2[i]);
+                    }
+                    for (int i = 0; i < dis2.Count(); i++)
+                    {
+                        y = (from a in ds.SYS_District
+                             where a.attachTo == dis2[i]
+                             select a).ToArray();
+                        for (int j = 0; j < y.Length; j++)
+                        {
+                            if (dis1.Contains(y[j].id) == false)
+                            {
+                                dis1.Add(y[j].id);
+                            }
+                        }
+                    }
+                } while (dis1.ToArray().Length != 0);
+                return districts;
+            }
+            catch (Exception ex)
+            {
+                return districts;
+            }
+        }
+
+        /*--------------经纬度计算两点距离----------*/
+        public static double latitudelongitude2Length(double x1, double y1, double x2, double y2)
+        {
+            try
+            {
+                var R = 6371000;//地球半径
+                double Length = R * Math.Acos(Math.Cos(y1) * Math.Cos(y2) * Math.Cos(x1 - x2) + Math.Sin(y1) * Math.Sin(y2));
+                return Length;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        /*--------------浏览器半斜距----------*/
+        public static double calculateSlant(int x, int y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) / 2;
+        }
+        /*--------------计算选择半径-------------*/
+        public static double calculateRadius(int explorX, int explorY, int zoomLevel)
+        {
+            var explorSlant = calculateSlant(explorX, explorY);
+            int scale = 0;
+            switch (zoomLevel)
+            {
+                case 2:
+                    scale = 5000;
+                    break;
+                case 3:
+                    scale = 5000;
+                    break;
+                case 4:
+                    scale = 3000;
+                    break;
+                case 5:
+                    scale = 1000;
+                    break;
+                case 6:
+                    scale = 500;
+                    break;
+                case 7:
+                    scale = 300;
+                    break;
+                case 8:
+                    scale = 200;
+                    break;
+                case 9:
+                    scale = 100;
+                    break;
+                case 10:
+                    scale = 50;
+                    break;
+                case 11:
+                    scale = 20;
+                    break;
+                default:
+                    scale = 0;
+                    break;
+            }
+            return scale * explorSlant * 0.015;
+        }
+
+        /*--------------坐标字符串转化为区域点集-------------*/
+        public static DataContracts.Map_District_Area[] areaStr2areaAssembly(string area)
+        {
+            if (!string.IsNullOrEmpty(area))
+            {
+                try
+                {
+                    area = area.Replace("\n", "");
+                    area = area.Substring(6);
+                    area = area.Remove(area.Length - 2, 2);
+                    area = area.Replace("),(", "P");
+                    string[] areaPoints = area.Split('P');
+                    DataContracts.Map_District_Area[] Points = new DataContracts.Map_District_Area[areaPoints.Length];
+                    for (int j = 0; j < areaPoints.Length; j++)
+                    {
+                        double Pointx, Pointy;
+                        DataContracts.Map_District_Area point = new DataContracts.Map_District_Area();
+                        string[] areaPointsCoordinate = areaPoints[j].Split(',');
+                        Pointx = Convert.ToDouble(areaPointsCoordinate[0]);
+                        Pointy = Convert.ToDouble(areaPointsCoordinate[1]);
+                        point.x = Pointx;
+                        point.y = Pointy;
+                        Points[j] = point;
+                    }
+                    return Points.ToArray();
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /*--------------基姆拉尔森计算公式计算日期变星期-------------*/
+        public static string CaculateWeekDay(int y, int m, int d)
+        {
+            if (m == 1 || m == 2)
+            {
+                m += 12;
+                y--;         
+                //把一月和二月看成是上一年的十三月和十四月，例：如果是2004-1-10则换算成：2003-13-10来代入公式计算。
+            }
+            int week = (d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7;
+            string weekstr = "";
+            switch (week)
+            {
+                case 0: weekstr = "星期一"; break;
+                case 1: weekstr = "星期二"; break;
+                case 2: weekstr = "星期三"; break;
+                case 3: weekstr = "星期四"; break;
+                case 4: weekstr = "星期五"; break;
+                case 5: weekstr = "星期六"; break;
+                case 6: weekstr = "星期日"; break;
+            }
+            return weekstr;
+        }
+    }
+}
---
+++ MLDSService/Methods/ExcelAnalysis.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using System.Reflection;
+using System.Web;
+using NPOI.SS.UserModel;
+using System.Collections;
+using MLDSData;
+
+namespace MLDSService.Methods
+{
+    public static class ExcelAnalysis
+    {
+        private static MLDSDataContext ds = new MLDSDataContext();
+        /*-------------房屋导入----------------*/
+        public static void excelSaveBuilding(List<DataContracts.Excel_Building> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_Building en = enlist[i];
+                //查询是否已存在此人
+                var thisBuilding = ds.POP_Building.SingleOrDefault(d => d.plot == en.plot && d.houseNum == en.houseNum && d.structure == en.structure && d.floor == en.floor && d.roomNum == en.roomNum);
+                if (thisBuilding == null)
+                {
+                    //新增
+                    var x = new POP_Building();
+                    x.id = Guid.NewGuid().ToString();
+                    x.districtID = en.districtID;
+                    x.plot = en.plot;
+                    x.houseNum = en.houseNum;
+                    x.x = en.x;
+                    x.y = en.y;
+                    x.structure = en.structure;
+                    x.floor = en.floor;
+                    x.roomNum = en.roomNum;
+                    ds.POP_Building.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+
+            //for (int i = 0; i < enlist.Count; i++)
+            //{
+            //    MLDSService.DataContracts.List_BasicPopulation enA = enlist[i];
+            //    if (enA.IsExcelVaildateOK == false) // 上面验证不通过，不进行此步验证
+            //    {
+            //        continue;
+            //    }
+
+            //    for (int j = i + 1; j < enlist.Count; j++)
+            //    {
+            //        MLDSService.DataContracts.List_BasicPopulation enB = enlist[j];
+            //        // 判断必填列是否全部重复
+            //        if (enA.Name == enB.Name)
+            //        {
+            //            enA.IsExcelVaildateOK = false;
+            //            enB.IsExcelVaildateOK = false;
+            //            errorMsg.AppendLine("第" + (i + 1) + "行与第" + (j + 1) + "行的必填列重复了");
+            //        }
+            //    }
+            //}
+            
+        }
+
+        /*-------------基础人口导入--------------*/
+        public static void excelSavePopulation(List<DataContracts.Excel_Population> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_Population en = enlist[i];
+                //查询是否已存在此人
+                var thisBuilding = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisBuilding == null)
+                {
+                    //新增
+                    var x = new POP_Basic();
+                    x.id = Guid.NewGuid().ToString();
+                    x.districtID = en.districtID;
+                    x.name = en.name;
+                    x.IDCard = en.IDCard;
+                    x.sex = en.sex;
+                    x.nation = en.nation;
+                    x.bookletNum = en.bookletNum;
+                    x.relationship = en.relationship;
+                    x.censusRegister = en.censusRegister;
+                    x.populationType = en.populationType;
+                    x.marriageStatus = en.marriageStatus;
+                    x.politicsStatus = en.politicsStatus;
+                    x.educationDegree = en.educationDegree;
+                    x.status = 1;
+                    x.phone = en.phone;
+                    x.workPlace = en.workPlace;
+                    var address = ds.POP_Building.Where(d => d.districtID == en.districtID && d.houseNum == en.houseNum).ToArray();
+                    if (address.Length > 0)
+                    {
+                        x.addressID = address[0].id;
+                    }
+                    if (x.relationship == "户主")
+                    {
+                        var y = new POP_Family();
+                        y.id = Guid.NewGuid().ToString();
+                        y.addressID = x.addressID;
+                        y.bookletNum = x.bookletNum;
+                        y.bookletType = x.populationType;
+                        y.districtID = x.districtID;
+                        y.houseHolderID = x.id;
+                        y.status = 1;
+                        ds.POP_Family.InsertOnSubmit(y);
+                    }
+                    ds.POP_Basic.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+
+        }
+
+        /*-------------党员导入---------------*/
+        public static void excelSavePartyMember(List<DataContracts.Excel_PartyMember> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_PartyMember en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new POP_Party();
+                    x.id = Guid.NewGuid().ToString();
+                    x.districtID = thisGuy.districtID;
+                    x.populationID = thisGuy.id;
+                    x.department = en.department;
+                    x.joinTime = en.joinTime;
+                    x.status = 1;
+                    ds.POP_Party.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+        }
+        /*-------------民兵导入---------------*/
+        public static void excelSaveMilitary(List<DataContracts.Excel_Military> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_Military en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new POP_Military();
+                    x.id = Guid.NewGuid().ToString();
+                    x.districtID = thisGuy.districtID;
+                    x.populationID = thisGuy.id;
+                    x.isActive = en.isActive;
+                    x.isBasic = en.isBasic;
+                    x.isDisable = en.isDisable;
+                    x.leaveTime = en.leaveTime;
+                    x.joinTime = en.joinTime;
+                    x.status = 1;
+                    ds.POP_Military.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+        }
+        /*-------------工作人员导入---------------*/
+        public static void excelSaveLeader(List<DataContracts.Excel_Leader> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_Leader en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new POP_Leader();
+                    x.id = Guid.NewGuid().ToString();
+                    x.districtID = thisGuy.districtID;
+                    x.populationID = thisGuy.id;
+                    x.duty = en.duty;
+                    x.status = 1;
+                    ds.POP_Leader.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+        }
+        /*-------------少儿医保导入---------------*/
+        public static void excelSaveChildrenInsurance(List<DataContracts.Excel_ChildrenInsurance> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_ChildrenInsurance en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new BUS_ChildrenInsurance();
+                    x.id = Guid.NewGuid().ToString();
+                    x.populationID = thisGuy.id;
+                    x.districtID = thisGuy.districtID;
+                    x.institutionID = en.institutionID;
+                    x.peopleID = en.peopleID;
+                    x.name = en.name;
+                    x.IDCard = en.IDCard;
+                    x.sex = en.sex;
+                    x.accountProperty = en.accountProperty;
+                    x.studentNum = en.studentNum;
+                    x.exemptionType = en.exemptionType;
+                    x.exemptionID = en.exemptionID;
+                    x.contract = en.contract;
+                    x.phone = en.phone;
+                    x.year = en.year;
+                    x.status = "1";
+                    ds.BUS_ChildrenInsurance.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+        }
+        /*-------------农村医疗导入---------------*/
+        public static void excelSaveRuralInsurance(List<DataContracts.Excel_RuralInsurance> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_RuralInsurance en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new BUS_RuralInsurance();
+                    x.id = Guid.NewGuid().ToString();
+                    x.populationID = thisGuy.id;
+                    x.districtID = thisGuy.districtID;
+                    x.peopleID = en.peopleID;
+                    x.name = en.name;
+                    x.IDCard = thisGuy.IDCard;
+                    x.sex = thisGuy.sex;
+                    x.exemptionType = en.exemptionType;
+                    x.exemptionID = en.exemptionID;
+                    x.phone = thisGuy.phone;
+                    x.year = en.year;
+                    x.status = "1";
+                    ds.BUS_RuralInsurance.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            ds.SubmitChanges();
+        }
+
+        /*-------------残疾人导入---------------*/
+        public static void excelSaveDisabled(List<DataContracts.Excel_Disabled> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_Disabled en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    //新增
+                    var x = new POP_Disabled();
+                    x.id = Guid.NewGuid().ToString();
+                    x.populationID = thisGuy.id;
+                    x.districtID = thisGuy.districtID;
+                    x.disableNum = en.disableNum;
+                    x.disablelevel = en.disablelevel;
+                    x.relieffunds = en.relieffunds;
+                    x.guardian = en.guardian;
+                    x.explain = en.explain;
+                    ds.POP_Disabled.InsertOnSubmit(x);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            ds.SubmitChanges();
+        }
+        /*-------------农田导入---------------*/
+        public static void excelSaveFarmLand(List<DataContracts.Excel_FarmLand> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_FarmLand en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                var thisFamily = new POP_Family();
+                var x = new AGR_FarmLand();
+                x.id = Guid.NewGuid().ToString();
+                x.address = en.address;
+                x.area = en.area;
+                x.farmLandID = en.farmLandID;
+                if (thisGuy != null)
+                {
+                    x.populationID = thisGuy.id;
+                    x.districtID = thisGuy.districtID;
+                    thisFamily = ds.POP_Family.SingleOrDefault(d => d.bookletNum == thisGuy.bookletNum && d.districtID == thisGuy.districtID);
+                    if (thisFamily != null)
+                    {
+                        x.familyID = thisFamily.id;
+                    }
+                }
+                ds.AGR_FarmLand.InsertOnSubmit(x);
+            }
+            ds.SubmitChanges();
+        }   
+        /*-------------楼栋二次匹配---------------*/
+        public static void excelFitBuilding(List<DataContracts.Excel_FitBuilding> enlist)
+        {
+            for (int i = 0; i < enlist.Count; i++)
+            {
+                DataContracts.Excel_FitBuilding en = enlist[i];
+                //查询是否已存在此人
+                var thisGuy = ds.POP_Basic.SingleOrDefault(d => d.name == en.name && d.IDCard == en.IDCard);
+                if (thisGuy != null)
+                {
+                    var thisBuilding = ds.POP_Building.SingleOrDefault(d => d.id == thisGuy.addressID);
+                    //var thisFamily = ds.POP_Family.SingleOrDefault(d => d.bookletNum == thisGuy.bookletNum);
+                    if (thisBuilding != null)
+                    {
+                        thisBuilding.x = en.x;
+                        thisBuilding.y = en.y;
+                    }
+                    else
+                    {
+                        var x = new POP_Building();
+                        x.id = Guid.NewGuid().ToString();
+                        x.districtID = thisGuy.districtID;
+                        x.x = en.x;
+                        x.y = en.y;
+                        x.structure = "1";
+                        x.houseNum = thisGuy.bookletNum;
+                        ds.POP_Building.InsertOnSubmit(x);
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            ds.SubmitChanges();
+        }
+    }
+}
